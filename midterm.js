c20e976292cd6eaa1a507339546beb594edb0360
marykatehfk <mhf273@nyu.edu>
2016-10-31 20:26:27 +0800

    added the sound background

--- midterm.js
+++ midterm.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", init);
 
 var force;
 var manybodyforce;
+var soundeffects = {};
 function init() {
     // initialized the force simulation
     var nodes = []
@@ -23,7 +24,7 @@ function init() {
     wordnodes.push({type:"w", radius: 35, text:"dog", _id:"dog", bounce:false});
     wordnodes.push({type:"w", radius: 35, text:"closure", _id:"closure", bounce:false});
     for (let i = 0; i < wordnodes.length; i++) {
-    	let width = getTextWidth(wordnodes[i].text, "35px Cormorant Garamond") / 2 + 1;
+    	let width = getTextWidth(wordnodes[i].text, "30px Cormorant Garamond") / 2 + 1;
     	wordnodes[i].radius =  width;
     	wordnodes[i].offset = -width;
     	nodes.push(wordnodes[i])
@@ -46,9 +47,9 @@ function init() {
     			  .force("boundingX", d3.forceX().strength(0.01))
     			  .force("boundingY", d3.forceY().strength(0.01))
     			  .on("tick", function(){doTick(nodes);})
-    			  .alphaMin(0)
-    			  .alphaDecay(0)
-    			  .alpha(0.6)
+    			  // .alphaMin(0)
+    			  .alphaDecay(0.01)
+    			  // .alpha(0.6)
     			  .restart();
 
     var color = d3.scaleOrdinal(d3.schemeCategory10);
@@ -66,10 +67,11 @@ function init() {
 			         .attr("cx", 0)
 			         .attr("cy", 0)
 			         .attr("r",  d.radius)
-			         .style("fill", rgbaFromHex(color(i%4), 0.85));
+			         .style("fill", rgbaFromHex(color(i%4), 0.7));
 	        if (d.type == "w") {
 	        	selection.select("circle")
-	        			 .style("fill", rgbaFromHex(color(i%4+4), 0.85));
+	        			 .style("fill", rgbaFromHex(color(i%4+4), 0.2))
+	        			 .style("stroke", "rgba(0,0,0,0.1)");
 
 	        	selection.append("text")
 					     .classed("wordshadow", true)
@@ -77,7 +79,7 @@ function init() {
 					     .attr("id", d._id + "_shadow")
 					     .attr("x", d.offset)
 					     .attr("y", 35/4)
-					     .attr("transform", "skewX(0)")
+					     .attr("transform", "skewX(0) translate(0)")
 					     .text(d.text);
 
 	        	selection.append("text")
@@ -86,13 +88,98 @@ function init() {
 					     .attr("id", d._id)
 					     .attr("x", d.offset)
 					     .attr("y", 35/4)
-					     .attr("transform", "skewX(0)")
+					     .attr("transform", "skewX(0) translate(0)")
 					     .text(d.text)
 					     .on("mouseover", mouseover)
 					     .on("click", doBounce);
 	        }
 	  	})
+	
+	// Load sound effects.
+	d3.json("sounds.json", function(e, d){
+		for (let i = 0; i < d.length; i++) {
+			let sound = d[i];
+			let soundname = sound.name;
+			let effectname = sound.effect;
+			let config = sound.config;
+			let filename = "resources/" + sound.filename;
+			let player = new Tone.Player(filename);
+			let effect;
+			// bitcrusher is a special snowflake and doesn't have optional variables
+			if (effectname == "BitCrusher") {
+				effect = new Tone.BitCrusher(config.bits);
+			}
+			else {
+				effect = new Tone[effectname]();
+				for (let key in config) {
+					if (typeof(effect[key]) != "object") {
+						effect[key] = config[key]
+					}
+					else {
+						effect[key].value = config[key];
+					}
+				}
+			}
+			
+			player.connect(effect);
+			effect.toMaster();
+			soundeffects[soundname + "_mod"] = player;
+			let player2 = new Tone.Player(filename);
+			soundeffects[soundname] = player2;
+		}
+	});
 
+	var synth = new Tone.SimpleSynth(
+		 {"oscillator":{
+		 "type":"triangle"
+		 },
+		 "envelope":{
+		 "attack": 0.001,
+		       "decay": 0.001,
+		       "sustain": 0.5,
+		       "release": 0.001
+		 }
+	});
+
+	synth.toMaster();
+	 
+	  
+	  var pattern = new Tone.Pattern(function(time, note){
+	      synth.triggerAttackRelease(note, 0.50);
+	  }, ["E3", "A3", "C4", "E4", "A4", "E4", "C4", "A3"]);
+	  
+	  pattern.start(0);
+	  Tone.Transport.bpm.value = 200;
+	  Tone.Transport.start();
+
+
+	  var kitPart = new Tone.Part(function(time, value){
+	  	soundeffects[value].start(time);
+	   
+	    },  [
+	          ["0:0:2", "babycry"],
+	          //["0:0:10", "dog"],
+	          ["0:2:3", "escalator"],
+	          
+	          
+	          ["0:4:0", "babycry"],
+	          ["0:5:3", "babycry"],
+	          ["5:0:0", "babycry"],
+	          ["5:0:", "tingting"],
+	          ["5:0:2", "tingting"],
+	          ["5:1:2", "tingting"],
+	         
+	          ["7:0:3", "dog"],
+	          
+	          ["9:0:0", "tingting_mod"]
+	         
+	      
+	          
+	        ]);
+	        
+	  kitPart.start(5);
+	  //kitPart.loop = true;
+	  Tone.Transport.start();  	
 }
 
 // Fisher-Yates shuffle
@@ -162,36 +249,73 @@ function doTick(nodes) {
 	});
 }
 
+// the stuff that happens when you click
 function doBounce(d, i){
+	force.alpha(1);
+	force.restart();
 	d.bounce = true;
 	setTimeout(function(){
 		d.bounce = false;
 	}, 500);
-}
 
-function sincurve(x) {
-	return Math.sin(x * 20 * Math.PI);
-}
-
-function mouseover(d) {
-	d3.select(this)
+	d3.select("#" + d._id + "_shadow")
 	  .transition()
-	  .ease(d3.easeCubicInOut)
+	  .ease(d3.easeExpOut)
 	  .duration(1000)
 	  .tween("wobble", function(){
 	  	var node = this;
 	  	return function(t) {
-	  		node.setAttribute("transform", "skewX(" +(0.5*sincurve(t)) + ")");
+	  		node.setAttribute("transform", transform_a(t, 1.5, 40));
 	  	}
 	  });
     d3.select("#" + d._id)
 	  .transition()
-	  .ease(d3.easeCubicInOut)
+	  .ease(d3.easeExpOut)
 	  .duration(1000)
 	  .tween("wobble", function(){
 	  	var node = this;
 	  	return function(t) {
-	  		node.setAttribute("transform", "skewX(" +(-0.5*sincurve(t)) + ")");
+	  		node.setAttribute("transform", transform_a(t, -1.5, 40));
+	  	}
+	  });
+}
+
+function sincurve(x, a) {
+	return Math.sin(x * a  * Math.PI);
+}
+
+function transform(t, n) {
+	return "skewX(" + (n * 0.5 * sincurve(t, 20)) + ") translate(" + (n * sincurve(t, 20)) + ")";
+}
+
+function transform_a(t, n, a) {
+	return "skewX(" + (n * 0.5 * sincurve(t, a)) + ") translate(" + (n * sincurve(t, a)) + ")";
+}
+
+function transform_skewonly(t, n, a) {
+	return "skewX(" + (n * 0.5 * sincurve(t, a)) + ")";
+}
+
+
+function mouseover(d) {
+	d3.select("#" + d._id)
+	  .transition()
+	  .ease(d3.easeExpInOut)
+	  .duration(1000)
+	  .tween("wobble", function(){
+	  	var node = this;
+	  	return function(t) {
+	  		node.setAttribute("transform", transform_a(t, 1, 20));
+	  	}
+	  });
+    d3.select("#" + d._id + "_shadow")
+	  .transition()
+	  .ease(d3.easeExpInOut)
+	  .duration(1000)
+	  .tween("wobble", function(){
+	  	var node = this;
+	  	return function(t) {
+	  		node.setAttribute("transform", transform_a(t, -1, 20));
 	  	}
 	  });
 }
